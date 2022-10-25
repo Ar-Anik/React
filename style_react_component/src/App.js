a8ba3fe30c6e33c37b6a60e0302124d6410ba6ca
Aubdur Rob Anik <aubdurrob1331@gmail.com>
2022-10-25 20:26:14 +0600

    10/25/2022

---
+++ style_react_component/src/App.js
@@ -0,0 +1,15 @@
+import './assets/css/global.css';
+import Logo from "./Components/Logo";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This Comes From App Class.</h1>
+
+      <Logo />
+
+    </div>
+  );
+}
+
+export default App;
