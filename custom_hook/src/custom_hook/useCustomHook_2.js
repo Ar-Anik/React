c32545eb2dcac969ff7ee360e3b66a11e8bedd5a
Aubdur Rob Anik <aubdurrob1331@gmail.com>
2022-10-31 18:12:42 +0600

    10/31/2022

--- custom_hook/src/custom_hook/useCustomHook_2.js
+++ custom_hook/src/custom_hook/useCustomHook_2.js
@@ -1,19 +1,42 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
+
+// const useCustomHook2 = (ScreenSize) => {
+//     const [scrnsize, setScrnsize] = useState(false);
+
+//     useEffect(() => {
+//         const ChangeScreen = () => {
+//             setScrnsize(window.innerWidth < ScreenSize);
+//         };
+
+//         ChangeScreen();
+
+//         window.addEventListener('resize', ChangeScreen);
+
+//         return () => window.removeEventListener('resize', ChangeScreen);
+//     }, [ScreenSize]);
+
+//     return scrnsize;
+// };
+
+// export default useCustomHook2;
+
+import { useCallback, useEffect, useState } from 'react';
 
 const useCustomHook2 = (ScreenSize) => {
     const [scrnsize, setScrnsize] = useState(false);
 
-    useEffect(() => {
-        const ChangeScreen = () => {
-            setScrnsize(window.innerWidth < ScreenSize);
-        };
+    const ChangeScreen = useCallback(() => {
+        setScrnsize(window.innerWidth < ScreenSize);
+    }, [ScreenSize]);
 
-        ChangeScreen();
+    useEffect(() => {
+        // eslint-disable-next-line no-unused-expressions
+        ChangeScreen;
 
         window.addEventListener('resize', ChangeScreen);
 
         return () => window.removeEventListener('resize', ChangeScreen);
-    }, [ScreenSize]);
+    }, [ChangeScreen]);
 
     return scrnsize;
 };
